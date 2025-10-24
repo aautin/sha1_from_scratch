25e3aca4d400fa2d6a939b93cff94d3fc13ca2ba
aautin <alexandreautin.aa@gmail.com>
2025-10-24 12:40:09 +0200

    fix file content len (dont consider /0 as the end)

--- inc/parsing.h
+++ inc/parsing.h
@@ -1,9 +1,11 @@
 #ifndef PARSING_H
 # define PARSING_H
 
+# include <unistd.h>
+
 # include "list.h"
 
-char		*get_content_from_fd(int fd);
+char		*get_content_from_fd(int fd, ssize_t *size);
 t_list		*get_msg_from_content(uint8_t* content, uint64_t len);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -20,14 +20,15 @@ int main(int argc, char** argv)
 		if (fd < 0)
 			perror(file);
 		else {
-			char*	content = get_content_from_fd(fd);
+			ssize_t content_size;
+			char*	content = get_content_from_fd(fd, &content_size);
 			close(fd);
 
 			if (content == NULL)
 				perror(file);
 			else {
 				uint32_t	hash[5];
-				if (!sha1((uint8_t*) content, (uint64_t) strlen(content) * 8, hash))
+				if (!sha1((uint8_t*) content, content_size * 8, hash))
 					perror(file);
 				else
 					printf("%08x%08x%08x%08x%08x  %s\n", hash[0], hash[1], hash[2], hash[3], hash[4], file);
